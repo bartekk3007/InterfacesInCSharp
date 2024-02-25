a1607248690b8dca9078217eb70e9bf7e4c3ddc5
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-02-25 23:39:29 +0100

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,20 @@
+﻿using Interface;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Teacher t1 = new Teacher("George", 50);
+        Student s1 = new Student("John", 20);
+
+        t1.SayPersonal();
+        s1.SayPersonal();
+
+        ((IPersonal)t1).Introduce();
+        ((IPersonal)s1).Introduce();
+
+        IPersonal i1 = new Teacher("Henry", 60);
+        i1.SayPersonal();
+        i1.Introduce();
+    }
+}
---
+++ Interface/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Teacher : IPersonal
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        public Teacher(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        void IPersonal.Introduce()
+        {
+            Console.WriteLine($"My name is {Name} and I am a teacher");
+        }
+        public void SayPersonal()
+        {
+            Console.WriteLine($"I am {Age} years old and I am a teacher");
+        }
+    }
+}
